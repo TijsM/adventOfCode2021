d84a5d86f98fb323f8c5c8c85fbd49481552d64c
Tijs Martens <tijs@bothrs.com>
2021-12-13 21:53:07 +0100

    feat: add data and file setup

---
+++ day4-giantSquid/index.ts
@@ -0,0 +1,22 @@
+import { Console } from "console";
+import fs from "fs";
+
+// Assignment
+// https://adventofcode.com/2021/day/4
+
+// GET THE DATA
+const input = fs
+  .readFileSync("day4-giantSquid/input.txt")
+  .toString()
+  .split("\n");
+
+const testInput = fs
+  .readFileSync("day4-giantSquid/testInput.txt")
+  .toString()
+  .split("\n");
+
+// TYPES
+
+// CODE
+
+console.log(testInput);
